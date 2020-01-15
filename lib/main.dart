5048da6f945b77bdf3e0755481817629560425da
Basem Saabneh <basemsa3abna@yahoo.com>
2020-01-15 23:48:15 +0200

    Flutter Sqflite Example CURD
    Signed-off-by: Basem Saabneh <basemsa3abna@yahoo.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:sqlite/ui/listview_employees.dart';
 
 void main() => runApp(MaterialApp(
-      title: '',
+      title: 'Sqflite Flutter ',
   home: ListViewEmployees(),
      ));
